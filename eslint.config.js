6adb26ece53c02eca4e57373ebf384f9367637c7
BLADE BAEK <qorqnseh@gmail.com>
2026-09-19 19:28:11 +0900

    chore(lint): internal/ 중첩 clone을 eslint 대상에서 제외

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import tseslint from 'typescript-eslint';
 export default tseslint.config(
-  { ignores: ['dist/**', 'node_modules/**'] },
+  { ignores: ['dist/**', 'node_modules/**', 'internal/**'] },
   ...tseslint.configs.recommended,
   { rules: { '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }] } },
 );
